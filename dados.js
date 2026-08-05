56767e8aa29585c4849d66ebc1cde3d4d63594c5
Evandro <129200446+soaresevandro@users.noreply.github.com>
2026-08-05 16:03:41 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -2,7 +2,7 @@ const dados = {
     noticias: [
         {
             id: 1,
-            imagem: "./img/img_abertura.png",
+            imagem: "img_abertura.png",
             "tag": " 4 de Agosto (HOJE)",
             "nome": "Após o recesso parlamentar, hoje haverá a sessão de abertura dos trabalhos legislativos!",
             "descricao": "É tempo de retomar debates, votações e ações que fortalecem a democracia e constroem o futuro do nosso município. Seguimos firmes no compromisso com o povo de Madre de Deus, trabalhando com responsabilidade, diálogo e transparência. 🤝",
@@ -10,7 +10,7 @@ const dados = {
         },
         {
             id: 2,
-            imagem: "./img/img_bahia.png",
+            imagem: "img_bahia.png",
             "tag": " 2 de Julho",
             "nome": "Consolidação da Independência da Bahia!",
             "descricao": "A coragem, a união e a determinação de homens e mulheres que lutaram pela liberdade deixaram um legado que atravessa gerações e fortalece nossa identidade",
@@ -18,7 +18,7 @@ const dados = {
         },
         {
             id: 3,
-            imagem: "./img/img_tiradentes.png",
+            imagem: "img_tiradentes.png",
             "tag": "21 de abril ",
             "nome": "Hoje é dia de lembrar que a liberdade vale a luta!",
             "descricao": "O Dia de Tiradentes representa coragem, resistência e o desejo por um país mais justo, valores que seguem vivos e necessários nos dias de hoje. Seguimos firmes, com responsabilidade e compromisso, trabalhando pelo presente e construindo o futuro do nosso povo.",
@@ -26,7 +26,7 @@ const dados = {
         },
         {
             id: 4,
-            imagem: "./img/img_autismo.png",
+            imagem: "img_autismo.png",
             "tag": "2 de abril",
             "nome": "Hoje é dia de reforçar algo essencial: respeito, empatia e inclusão!",
             "descricao": "No Dia Mundial da Conscientização do Autismo, lembramos que cada pessoa é única, com seu jeito especial de ver e sentir o mundo. Que possamos construir, juntos, um ambiente mais acolhedor, com mais compreensão e oportunidades para todos. ",
@@ -34,7 +34,7 @@ const dados = {
         },
         {
             id: 5,
-            imagem: "./img/img_vereador.png",
+            imagem: "img_vereador.png",
             "tag": "1 de outubro",
             "nome": "Hoje celebramos o Dia do Vereador(a)!",
             "descricao": "É uma data para valorizar o compromisso diário com o diálogo, a transparência e a construção de uma cidade melhor para todos. Parabéns a todos os vereadores e vereadoras!",
